3ec5a5da7e2a858fef6543c165721ed78b6028f0
Tuncaysukan <tuncay_sukan@hotmail.com>
2021-03-27 01:02:05 +0300

    the end
    the end

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+const circle = document.getElementById('circle')
+const upBtn = document.getElementById('upBtn')
+const downBtn = document.getElementById('downBtn')
+
+var rotateValue= circle.style.transform;
+var rotateSum;
+
+upBtn.onclick=function()
+{
+  rotateSum = rotateValue + "rotate(-90deg)";
+  circle.style.transform=rotateSum;
+  rotateValue=rotateSum;
+  console.log(rotateValue);
+}
+
+downBtn.onclick = function () {
+  rotateSum = rotateValue + "rotate(90deg)";
+  circle.style.transform = rotateSum;
+  rotateValue = rotateSum;
+  console.log(rotateValue);
+}
